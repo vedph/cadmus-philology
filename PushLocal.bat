7a579e293aa1e4d1ec414317f32955d642798ae9
Myrmex <dfusi@hotmail.com>
2023-09-04 12:28:25 +0200

    Updated packages

--- PushLocal.bat
+++ PushLocal.bat
@@ -1,6 +1,6 @@
 @echo off
 echo PRESS ANY KEY TO INSTALL TO LOCAL NUGET FEED
 echo Remember to generate the up-to-date package.
-c:\exe\nuget add .\Cadmus.Philology.Parts\bin\Debug\Cadmus.Philology.Parts.7.0.2.nupkg -source C:\Projects\_NuGet
-c:\exe\nuget add .\Cadmus.Seed.Philology.Parts\bin\Debug\Cadmus.Seed.Philology.Parts.7.0.2.nupkg -source C:\Projects\_NuGet
+c:\exe\nuget add .\Cadmus.Philology.Parts\bin\Debug\Cadmus.Philology.Parts.7.0.3.nupkg -source C:\Projects\_NuGet
+c:\exe\nuget add .\Cadmus.Seed.Philology.Parts\bin\Debug\Cadmus.Seed.Philology.Parts.7.0.3.nupkg -source C:\Projects\_NuGet
 pause
--- README.md
+++ README.md
@@ -6,6 +6,10 @@ This solution contains the Cadmus philologic parts library derived from `Cadmus.
 
 ## History
 
+### 7.0.3
+
+- 2023-09-04: updated packages.
+
 ### 7.0.2
 
 - 2023-06-16: updated packages.
