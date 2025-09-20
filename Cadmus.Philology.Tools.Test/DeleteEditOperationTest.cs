81bd38f97c2f18c177b4e766d7c7d75df7e70ff5
Myrmex <dfusi@hotmail.com>
2025-09-20 12:54:24 +0200

    More tests

--- Cadmus.Philology.Tools.Test/DeleteEditOperationTest.cs
+++ Cadmus.Philology.Tools.Test/DeleteEditOperationTest.cs
@@ -1,16 +1,154 @@
-﻿namespace Cadmus.Philology.Tools.Test;
+﻿using Xunit;
+
+namespace Cadmus.Philology.Tools.Test;
 
 public sealed class DeleteEditOperationTest
 {
     [Fact]
-    public void Execute_SingleChar()
+    public void Execute_SingleChar_Middle()
     {
         DeleteEditOperation op = (DeleteEditOperation)
-            EditOperation.ParseOperation("3!")!;
+            EditOperation.ParseOperation("@3!")!;
         const string a = "coniunx";
 
         string b = op.Execute(a);
 
         Assert.Equal("coiunx", b);
+    }
+
+    [Fact]
+    public void Execute_SingleChar_Start()
+    {
+        DeleteEditOperation op = (DeleteEditOperation)
+            EditOperation.ParseOperation("@1!")!;
+        const string a = "coniunx";
+
+        string b = op.Execute(a);
+
+        Assert.Equal("oniunx", b);
+    }
+
+    [Fact]
+    public void Execute_SingleChar_End()
+    {
+        DeleteEditOperation op = (DeleteEditOperation)
+            EditOperation.ParseOperation("@7!")!;
+        const string a = "coniunx";
+
+        string b = op.Execute(a);
+
+        Assert.Equal("coniun", b);
+    }
+
+    [Fact]
+    public void Execute_MultiChar_Middle()
+    {
+        DeleteEditOperation op = (DeleteEditOperation)
+            EditOperation.ParseOperation("@3x2!")!;
+        const string a = "coniunx";
+
+        string b = op.Execute(a);
+
+        Assert.Equal("counx", b);
+    }
+
+    [Fact]
+    public void Execute_MultiChar_Start()
+    {
+        DeleteEditOperation op = (DeleteEditOperation)
+            EditOperation.ParseOperation("@1x2!")!;
+        const string a = "coniunx";
+
+        string b = op.Execute(a);
+
+        Assert.Equal("niunx", b);
+    }
+
+    [Fact]
+    public void Execute_MultiChar_End()
+    {
+        DeleteEditOperation op = (DeleteEditOperation)
+            EditOperation.ParseOperation("@6x2!")!;
+        const string a = "coniunx";
+
+        string b = op.Execute(a);
+
+        Assert.Equal("coniu", b);
+    }
+
+    [Fact]
+    public void Parse_Dsl_QuotedText_Position()
+    {
+        DeleteEditOperation op = new();
+        op.Parse("\"abc\"@2!");
+
+        Assert.Equal("abc", op.InputText);
+        Assert.Equal(2, op.Position);
+        Assert.Equal(1, op.Length);
+    }
+
+    [Fact]
+    public void Parse_Dsl_QuotedText_Position_Length()
+    {
+        DeleteEditOperation op = new();
+        op.Parse("\"abc\"@2x3!");
+
+        Assert.Equal("abc", op.InputText);
+        Assert.Equal(2, op.Position);
+        Assert.Equal(3, op.Length);
+    }
+
+    [Fact]
+    public void Parse_Dsl_Position_Only()
+    {
+        DeleteEditOperation op = new();
+        op.Parse("@4!");
+
+        Assert.Null(op.InputText);
+        Assert.Equal(4, op.Position);
+        Assert.Equal(1, op.Length);
+    }
+
+    [Fact]
+    public void Parse_Dsl_Position_Length_x()
+    {
+        DeleteEditOperation op = new();
+        op.Parse("@4x2!");
+
+        Assert.Null(op.InputText);
+        Assert.Equal(4, op.Position);
+        Assert.Equal(2, op.Length);
+    }
+
+    [Fact]
+    public void Parse_Dsl_Position_Length_times()
+    {
+        DeleteEditOperation op = new();
+        op.Parse("@4×2!");
+
+        Assert.Null(op.InputText);
+        Assert.Equal(4, op.Position);
+        Assert.Equal(2, op.Length);
+    }
+
+    [Fact]
+    public void Parse_InvalidFormat_Throws()
+    {
+        DeleteEditOperation op = new();
+        Assert.Throws<ParseException>(() => op.Parse("invalid!"));
+    }
+
+    [Fact]
+    public void Parse_InvalidPosition_Throws()
+    {
+        DeleteEditOperation op = new();
+        Assert.Throws<ParseException>(() => op.Parse("@0!"));
+    }
+
+    [Fact]
+    public void Parse_InvalidLength_Throws()
+    {
+        DeleteEditOperation op = new();
+        Assert.Throws<ParseException>(() => op.Parse("@2x0!"));
     }
 }
---
+++ Cadmus.Philology.Tools.Test/InsertAfterEditOperationTest.cs
@@ -0,0 +1,142 @@
+﻿using Xunit;
+using System;
+
+namespace Cadmus.Philology.Tools.Test;
+
+public sealed class InsertAfterEditOperationTest
+{
+    [Fact]
+    public void Execute_InsertAtStart()
+    {
+        InsertAfterEditOperation op = new()
+        {
+            Position = 0,
+            Text = "abc"
+        };
+        const string a = "xyz";
+
+        string b = op.Execute(a);
+
+        Assert.Equal("xyzabc", b);
+    }
+
+    [Fact]
+    public void Execute_InsertInMiddle()
+    {
+        InsertAfterEditOperation op = new()
+        {
+            Position = 2,
+            Text = "abc"
+        };
+        const string a = "xyz";
+
+        string b = op.Execute(a);
+
+        Assert.Equal("xyabcz", b);
+    }
+
+    [Fact]
+    public void Execute_InsertAtEnd()
+    {
+        InsertAfterEditOperation op = new()
+        {
+            Position = 3,
+            Text = "abc"
+        };
+        const string a = "xyz";
+
+        string b = op.Execute(a);
+
+        Assert.Equal("xyzabc", b);
+    }
+
+    [Fact]
+    public void Execute_NullInput_Throws()
+    {
+        InsertAfterEditOperation op = new()
+        {
+            Position = 1,
+            Text = "abc"
+        };
+
+        Assert.Throws<ArgumentNullException>(() => op.Execute(null!));
+    }
+
+    [Fact]
+    public void Execute_InvalidPosition_Throws()
+    {
+        InsertAfterEditOperation op = new()
+        {
+            Position = 5,
+            Text = "abc"
+        };
+        const string a = "xyz";
+
+        Assert.Throws<ArgumentOutOfRangeException>(() => op.Execute(a));
+    }
+
+    [Fact]
+    public void Parse_ValidFormat()
+    {
+        InsertAfterEditOperation op = new();
+        op.Parse("@2=+\"abc\"");
+
+        Assert.Equal(2, op.Position);
+        Assert.Equal("abc", op.Text);
+    }
+
+    [Fact]
+    public void Parse_ValidFormat_ZeroPosition()
+    {
+        InsertAfterEditOperation op = new();
+        op.Parse("@0=+\"abc\"");
+
+        Assert.Equal(0, op.Position);
+        Assert.Equal("abc", op.Text);
+    }
+
+    [Fact]
+    public void Parse_InvalidFormat_Throws()
+    {
+        InsertAfterEditOperation op = new();
+        Assert.Throws<ParseException>(() => op.Parse("invalid!"));
+    }
+
+    [Fact]
+    public void Parse_NegativePosition_Throws()
+    {
+        InsertAfterEditOperation op = new();
+        Assert.Throws<ParseException>(() => op.Parse("@-1=+\"abc\""));
+    }
+
+    [Fact]
+    public void ToString_ReturnsExpected()
+    {
+        InsertAfterEditOperation op = new()
+        {
+            Position = 2,
+            Text = "abc"
+        };
+
+        string s = op.ToString();
+
+        Assert.Equal("@2=+\"abc\"", s);
+    }
+
+    [Fact]
+    public void ToString_WithNoteAndTags()
+    {
+        InsertAfterEditOperation op = new()
+        {
+            Position = 2,
+            Text = "abc",
+            Note = "note"
+        };
+        op.Tags.Add("t1");
+        op.Tags.Add("t2");
+
+        string s = op.ToString();
+
+        Assert.Equal("@2=+\"abc\" (note) [t1 t2]", s);
+    }
+}
